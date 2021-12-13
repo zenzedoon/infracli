071935202a902961aa4bfe52a4366a3c0a4952ac
Zied BEN SALEM <zbensalem@mantu.com>
2021-12-13 22:13:09 +0100

    second try

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
   subscription_id = "fa287b8e-5fd5-49d6-a631-0a6f08210141"
-  client_id       = var_clientid  #
-  client_secret   = var_client_secret #
+  client_id       =  TF_VAR_clientid  #
+  client_secret   = TF_VAR_client_secret #
   tenant_id       = "ccee54f2-1557-45ce-81d7-23b703516da2"
 }
